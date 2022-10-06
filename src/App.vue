78b281d389f8c2f82be0c2b18c4518612d6e2a2b
Akshata9986 <akshatanaik9164@gmail.com>
2022-10-06 13:42:06 +0530

    Vue_Routing_Navigation_Guards

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar-comp />
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavbarComp from "./components/NavbarComp.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { NavbarComp },
+};
 </script>
 
 <style>
